0eead79eea2cc863bf65d8b2ec8149f934239c72
Nick Saunders <nick@saunde.rs>
2020-10-19 20:19:41 -0700

    Customize purescript download

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for purs.
 GH_REPO="https://github.com/purescript/purescript"
 
 fail() {
@@ -12,7 +11,6 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if purs is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
   curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -25,22 +23,28 @@ sort_versions() {
 list_github_tags() {
   git ls-remote --tags --refs "$GH_REPO" |
     grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+    sed 's/^v//'
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if purs has other means of determining installable versions.
   list_github_tags
 }
 
 download_release() {
-  local version filename url
+  local version filename bin url
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for purs
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+    bin="linux64"
+  elif [[ "$OSTYPE" == "darwin"* ]]; then
+    bin="macos"
+  else
+    fail "unrecognized operating system $OSTYPE"
+  fi
+
+  # https://github.com/purescript/purescript/releases/download/v0.13.8/linux64.tar.gz
+  url="$GH_REPO/releases/download/v${version}/${bin}.tar.gz"
 
   echo "* Downloading purs release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -55,15 +59,13 @@ install_version() {
     fail "asdf-purs supports release installs only"
   fi
 
-  # TODO: Adapt this to proper extension and adapt extracting strategy.
   local release_file="$install_path/purs-$version.tar.gz"
   (
     mkdir -p "$install_path"
     download_release "$version" "$release_file"
-    tar -xzf "$release_file" -C "$install_path" --strip-components=1 || fail "Could not extract $release_file"
+    tar -xzf "$release_file" -C "$install_path" --strip-components=1 purescript/purs || fail "Could not extract $release_file"
     rm "$release_file"
 
-    # TODO: Asert purs executable exists.
     local tool_cmd
     tool_cmd="$(echo "purs --help" | cut -d' ' -f2-)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
